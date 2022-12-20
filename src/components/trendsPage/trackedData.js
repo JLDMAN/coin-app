733d56bbcebc590dffe8a5e4def60d8b83a7a937
drikus <viljoend0@gmail.com>
2022-12-20 20:12:14 +0200

    Dynamic sizing of graphs based on window size

--- src/components/trendsPage/trackedData.js
+++ src/components/trendsPage/trackedData.js
@@ -46,11 +46,31 @@ function Tracked(props){
             )
         })
 
+        // check screen size to adjust chart sizing
+        const [screenSize, getDimension] = useState({
+            dynamicWidth: window.innerWidth,
+            dynamicHeight: window.innerHeight
+          });
+          const setDimension = () => {
+            getDimension({
+              dynamicWidth: window.innerWidth,
+              dynamicHeight: window.innerHeight
+            })
+          }
+          
+          useEffect(() => {
+            window.addEventListener('resize', setDimension);
+            
+            return(() => {
+                window.removeEventListener('resize', setDimension);
+            })
+          }, [screenSize])
+
     return (
         <div className="center" style={{marginTop:'16%'}}>
             <div className="space">
                 <p style={{color: 'gray'}}>Price Per Coin (USD)</p>
-                <LineChart width={600} height={300} data={changingPrices} margin={{left: 80, top: 50, bottom: 50}}>
+                <LineChart width={0.4*screenSize.dynamicWidth} height={0.5*screenSize.dynamicHeight} data={changingPrices} margin={{left: 80, top: 50, bottom: 50}}>
                     <Line type="monotone" dataKey="value" stroke="#40B3B3" strokeWidth={2} dot={false}/>
                     <YAxis domain={[Math.min(changingPrices), Math.max(changingPrices)]}/>
                     <Tooltip />
@@ -58,19 +78,25 @@ function Tracked(props){
             </div>
             <div className="space">
                 <p style={{color: 'gray'}}>Market Cap (USD in Millions)</p>
-                <LineChart width={600} height={300} data={changingMarketCap} margin={{left: 80, top: 100, bottom: 50}}>
+                <LineChart width={0.4*screenSize.dynamicWidth} height={0.5*screenSize.dynamicHeight} data={changingMarketCap} margin={{left: 80, top: 100, bottom: 50}}>
                     <Line type="monotone" dataKey="value" stroke="#40B3B3" strokeWidth={2} dot={false}/>
                     <YAxis domain={[Math.min(changingMarketCap), Math.max(changingMarketCap)]}/>
                 </LineChart>
             </div>
             <div className="space bottom">
                 <p style={{color: 'gray'}}>Trading Volume (USD in Millions)</p>
-                <LineChart width={600} height={300} data={changingTradeVolume} margin={{left: 80, top: 100, bottom: 50}}>
+                <LineChart width={0.4*screenSize.dynamicWidth} height={0.5*screenSize.dynamicHeight} data={changingTradeVolume} margin={{left: 80, top: 100, bottom: 50}}>
                     <Line type="monotone" dataKey="value" stroke="#40B3B3" strokeWidth={2} dot={false}/>
                     <YAxis />
                     {/* domain={[(Math.min(changingTradeVolume)), (Math.max(changingTradeVolume))]} */}
                 </LineChart>
             </div>
+            {/* <div>
+                <ul>
+                    <li>Width: <strong>{screenSize.dynamicWidth}</strong></li>
+                    <li>Height: <strong>{screenSize.dynamicHeight}</strong></li>
+                </ul>    
+            </div> */}
         </div>
     );
 }
